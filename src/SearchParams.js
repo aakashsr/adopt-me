a0e3cf4b7f616d3b83e3a3c944064b0f5f5b120d
aakashsr <aakashsri.dev@gmail.com>
2020-06-06 08:23:57 +0530

    created select element and created hook for it too

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   // const location = "Seatle, WA";
   const [location, setLocation] = useState("Seatle , WA");
+  const [animal, setAnimal] = useState("Dog");
   return (
     <div className="search-params">
       <form>
@@ -14,6 +16,20 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
